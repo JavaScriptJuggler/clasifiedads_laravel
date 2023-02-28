e6439f5170aa8b1da7759c4dafa01437f6f72a53
Soumya Manna <darkprience1998@gmail.com>
2023-02-28 19:56:14 +0530

    bread crumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,6 +2,7 @@
 
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+use Illuminate\Support\Facades\Request;
 
 Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail): void {
     $trail->push('Dashboard', route('dashboard'));
@@ -9,4 +10,8 @@ Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail): void {
 Breadcrumbs::for('ads.ads-list', function (BreadcrumbTrail $trail): void {
     $trail->parent('dashboard');
     $trail->push('My Ads', route('ads.ads-list'));
+});
+Breadcrumbs::for('ads.view-ads', function (BreadcrumbTrail $trail, $recordid): void {
+    $trail->parent('ads.ads-list');
+    $trail->push('View Ads', route('ads.view-ads', ['recordid' => $recordid]));
 });
