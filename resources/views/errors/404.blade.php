ffe6025f4fafb0e374d4584debbd0f79aa81d117
Soumya Manna <darkprience1998@gmail.com>
2023-03-05 09:57:41 +0530

    404 error page change

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,7 +12,7 @@
         <div class="misc-wrapper">
             <h2 class="mb-2 mx-2">Page Not Found :(</h2>
             <p class="mb-4 mx-2">Oops! 😖 The requested URL was not found on this server.</p>
-            <a href="{{ route('dashboard') }}" class="btn btn-primary">Back to home</a>
+            <a href="{{ route('dashboard') }}" class="btn btn-primary">Back to dashboard</a>
             <div class="mt-3">
                 <img src="{{ asset('assets/img/illustrations/page-misc-error-light.png') }}" alt="page-misc-error-light"
                     width="500" class="img-fluid" data-app-dark-img="illustrations/page-misc-error-dark.png"
